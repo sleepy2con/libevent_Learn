883661cbcc8cb1a3fa16fc2b33129c6d15dcfa24
hyx <heyuxuan3985@outlook.com>
2025-05-17 12:58:19 +0800

    创建一个废弃分支，废弃曾经的练习代码

--- libevent-httpPrac/pub.h
+++ libevent-httpPrac/pub.h
@@ -8,15 +8,6 @@
 #include <evhttp.h>
 #include <event.h>
 #include <string.h>
-#include "event2/http.h"
-#include "event2/event.h"
-#include "event2/buffer.h"
-#include "event2/bufferevent.h"
-#include "event2/bufferevent_compat.h"
-#include "event2/http_struct.h"
-#include "event2/http_compat.h"
-#include "event2/util.h"
-#include "event2/listener.h"
 #include "cJSON.h"
 enum
 {
