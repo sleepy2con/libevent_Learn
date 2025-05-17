27aae6f214296e5db2b14d2749f7036ccb4be89c
hyx <heyuxuan3985@outlook.com>
2025-05-17 20:11:47 +0800

    写一个简单的阻塞socket程序

--- block_io_test/block_io_test.cpp
+++ block_io_test/block_io_test.cpp
@@ -3,36 +3,121 @@
 
 #include <iostream>
 #include <string>
-//#include <winsock2.h>
-#include <ws2tcpip.h> // For getaddrinfo (recommended)
-//#include <windows.h>  // For Sleep (替代 usleep，如果需要)
 #include <stdio.h>
 
-#pragma comment(lib, "Ws2_32.lib")
-#pragma comment(lib, "")
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h> // For getaddrinfo (recommended)
+// MSVC专用的编译器指令，在链接阶段，自动添加名为 Ws2_32.lib 的库文件，不需要你手动去项目设置里添加
+#pragma comment(lib, "Ws2_32.lib") // 配合ws2tcpip.h；
+#endif
+
+// 为了避免每次退出都得调用WSACleanup
+struct WSA_wraper
+{
+	WSADATA wsadata; // 存储初始化信息
+	bool valid = false;
+	WSA_wraper() {
+		// return 0成功
+		// windows必须借此初始化Winsock 库
+		int result = WSAStartup(MAKEWORD(2, 2), &wsadata);
+		valid = (result == 0);
+		if (!valid)
+		{
+			std::cout << "WSAStartup failed: " << result << std::endl;
+		}
+	};
+	~WSA_wraper() { WSACleanup(); };
+};
+
+
+//#include <windows.h>  // For Sleep (替代 usleep，如果需要)
 int main()
 {
+	// wchar_t 宽字符，L是宽字符前缀，表示后面的字符串是宽字符（wchar_t）类型的字符串，而不是普通的多字节（char）字符串。
+	const wchar_t  localhost_str[] = L"127.0.0.1";
+	const u_short listen_port = 8080;
 
-	/*const char query[] = 
-		"GET / HTTP/1.0\r\n"
-		"Host: www.baidu.com\r\n"
-		"\r\n";
-	const char hostname[] = "www.baidu.com";
-	struct sockaddr_in sin;
-	struct hostent *h;
-	const char* cp;
-	int fd;
-	ssize_t n_written, remaining;*/
+	char path[MAX_PATH] = { 0 };
+	// 获得当前程序名称
+	GetModuleFileNameA(nullptr, path, MAX_PATH);
+	//std::cout << path << std::endl;
 
+	// 初始化 Winsock 库.Windows 上必须手动初始化 Winsock 库，Linux/Unix 系统不需要
+	WSA_wraper wsa;
+	if (!wsa.valid) return 1;
+	//std::cout << "WSAStartup 成功" << std::endl;
+	// 创建Socket，一个文件描述符/句柄
+	int listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		std::cout << "socket failed: " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 1;
+	}
+
+	sockaddr_in service;
+	service.sin_family = AF_INET;	// 用来指定地址类型（也叫地址族）
+	// 主机字节序：有的机器是小端序（Intel x86 是小端），有的是大端序
+	// 网络字节序：网络协议规定是 大端序（高位字节在前）
+	service.sin_port = htons(listen_port); // 把主机字节序（Host Byte Order）的 16 位整数转换成网络字节序（Network Byte Order）
+	InetPton(AF_INET, localhost_str, &service.sin_addr);
+	if (bind(listenSocket, (sockaddr*)&service, sizeof(service)) == SOCKET_ERROR)
+	{
+		std::cout << "Bind failed: " << WSAGetLastError() << "\n";
+		closesocket(listenSocket);
+		return 1;
+	}
+	// backlog 表示监听队列的最大长度，表示操作系统允许挂起（排队等待）的未处理连接请求的最大数目
+	if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+	{
+		std::cout << "listen failed:" << WSAGetLastError() << "\n";
+		return 1;
+	}
+	std::cout << "waiting for client to connect..." << std::endl;
+
+	const int msgSize = 512;
+	char recvBuffer[msgSize];
+	char sendBuffer[msgSize];
+	while (1)
+	{
+		SOCKET clientSocket = accept(listenSocket, nullptr, nullptr);
+		if (clientSocket == -1) {
+			std::cout << "Accept clientSocket failed: " << WSAGetLastError() << std::endl;
+			closesocket(listenSocket);
+			return 1;
+		}
+		std::cout << "\nA new client accepted!" << std::endl;
+		while (1)
+		{
+			// 正整数：表示实际收到的字节数
+			int recvResult = recv(clientSocket, recvBuffer, sizeof(recvBuffer) - 1, 0);
+			if (recvResult > 0) {
+				recvBuffer[recvResult] = '\0';
+				if (strncmp(recvBuffer, "exit",4) == 0) {
+					std::cout << "Client requested to close connection." << std::endl;
+					break;
+				}
+				std::cout << "Client says: " << recvBuffer << std::endl;
+				// 你可以在这里输入要回复给客户端的内容
+				std::cout << "Enter reply: ";
+				std::cin.getline(sendBuffer, sizeof(sendBuffer));
+				//std::cout << "server send: " << sendBuffer << std::endl;
+				// 发送回复
+				send(clientSocket, sendBuffer, (int)strlen(sendBuffer), 0);
+			}
+			else {
+				std::cout << "Client connect failed." << std::endl;
+				break;
+			}
+		}
+		closesocket(clientSocket);
+	}
+
+	//  关闭 sockets
+	closesocket(listenSocket);
+	std::cin.get();
+	return 1;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
