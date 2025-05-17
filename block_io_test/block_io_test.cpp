0db26ac49debd2bfacaa84287e7e353f606a5b2d
hyx <heyuxuan3985@outlook.com>
2025-05-17 13:05:19 +0800

    添加了新的socket练习项目文件

---
+++ block_io_test/block_io_test.cpp
@@ -0,0 +1,38 @@
+﻿// block_io_test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+//#include <winsock2.h>
+#include <ws2tcpip.h> // For getaddrinfo (recommended)
+//#include <windows.h>  // For Sleep (替代 usleep，如果需要)
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+#pragma comment(lib, "")
+int main()
+{
+
+	/*const char query[] = 
+		"GET / HTTP/1.0\r\n"
+		"Host: www.baidu.com\r\n"
+		"\r\n";
+	const char hostname[] = "www.baidu.com";
+	struct sockaddr_in sin;
+	struct hostent *h;
+	const char* cp;
+	int fd;
+	ssize_t n_written, remaining;*/
+
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ simpleSocket/simpleSocket.cpp
@@ -0,0 +1,104 @@
+﻿// simpleSocket.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <WS2tcpip.h>
+#include <WinSock2.h>
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+	// 一个简单的socket程序
+	// socket()创建监听socket
+	//  bind()绑定IP + 端口
+	//	listen()开始监听
+	//	accept()接收一个客户端连接，得到新的clientSocket
+	//	用clientSocket去send()和recv()
+
+	// 测试 telnet 127.0.0.1 8080
+	const wchar_t  localhost_str[] = L"127.0.0.1";
+	const u_short listen_port = 8080;
+
+	char path[MAX_PATH] = { 0 };
+	GetModuleFileNameA(NULL, path, MAX_PATH);
+	std::cout << path << std::endl;
+	WSADATA wsadata;
+	int result = WSAStartup(MAKEWORD(2, 2), &wsadata);
+	if (result != 0)
+	{
+		std::cout << "WSAStartup failed: " << result << std::endl;
+		return 1;
+	}
+	// 创建socket
+	SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (listenSocket == INVALID_SOCKET)
+	{
+		std::cout << "Socket failed: " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		return 1;
+	}
+	// 3. 绑定地址和端口
+	sockaddr_in service;
+	service.sin_family = AF_INET;
+	//service.sin_addr.s_addr = InetPton("127.0.0.1"); // 本地回环
+	service.sin_port = htons(listen_port); // 监听8080端口
+	// 使用 InetPton 替换 inet_addr
+	InetPton(AF_INET, localhost_str, &service.sin_addr);
+	if (bind(listenSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+		std::cout << "Bind failed: " << WSAGetLastError() << "\n";
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+	
+	// 4. 监听端口
+	if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
+		std::cout << "Listen failed: " << WSAGetLastError() << std::endl;
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+	std::cout << "Waiting for client to connect..." << std::endl;
+	// 5. 接受客户端连接
+	SOCKET clientSocket = accept(listenSocket, NULL, NULL);
+	if (clientSocket == INVALID_SOCKET) {
+		std::cout << "Accept clientSocket failed: " << WSAGetLastError() << std::endl;
+		closesocket(listenSocket);
+		WSACleanup();
+		return 1;
+	}
+	std::cout << "Client connected!" << std::endl;
+
+	char recvBuffer[512];
+	char sendBuffer[512];
+	while (true) {
+		int recvResult = recv(clientSocket, recvBuffer, sizeof(recvBuffer) - 1, 0);
+		if (recvResult > 0) {
+			recvBuffer[recvResult] = '\0';
+			std::cout << "Client says: " << recvBuffer << std::endl;
+
+			// 你可以在这里输入要回复给客户端的内容
+			std::cout << "Enter reply: ";
+			std::cin.getline(sendBuffer, sizeof(sendBuffer));
+			std::cout << "server send: " << sendBuffer << std::endl;
+			// 发送回复
+			send(clientSocket, sendBuffer, (int)strlen(sendBuffer), 0);
+		}
+		else if (recvResult == 0) {
+			std::cout << "Client disconnected." << std::endl;
+			break;  
+		}
+		else {
+			std::cout << "recv failed: " << WSAGetLastError() << std::endl;
+			break;
+		}
+	}
+
+	//  关闭 sockets
+	closesocket(clientSocket);
+	closesocket(listenSocket);
+
+	WSACleanup(); // 清理Winsock
+
+	return 0;
+}
