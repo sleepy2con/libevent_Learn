a1aa665a26b27a2c03ffb1b30b9a36cb86542ee2
hyx <heyuxuan_work@qq.com>
2025-05-28 22:20:11 +0800

    添加了libevent的低级版本程序，但未完成，我现在想把远程仓库拉下来但是需要先commit，我被迫commit

--- libevent-hello-world/hello_world.cpp
+++ libevent-hello-world/hello_world.cpp
@@ -6,13 +6,22 @@
 
   Where possible, it exits cleanly in response to a SIGINT (ctrl-c).
 */
-//#include <iostream>
+#include <iostream>
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
 #include <event2/listener.h>
 #include <event2/util.h>
 #include <event2/event.h>
 //#include "pch.h"
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#pragma comment(lib, "ws2_32.lib")
+
+static void listener_cb(struct evconnlistener* evutil_socket_t,
+	struct sockaddr*, int socklen, void*);
+
+static const int port = 9995;
 
 int main(int argc, char** argv)
 {
@@ -20,14 +29,32 @@ int main(int argc, char** argv)
 	// 他能让你知道套接字何时准备好读取或写入,执行基本的超时功能,并检测操作系统信号
 
 	struct event_base* base;
-	struct evconnlistenner* listener;
+	struct evconnlistener* listener;
+	struct event* signal_event;
+
+	// 结构体被初始化为0，这表示sockaddr_in的sin_addr被设置为
+	// INADDR_ANY，即 0.0.0.0，所有本地网卡。
+	struct sockaddr_in sockaddr_in = { 0 }; 
 #ifdef _WIN32
 	WSADATA wsa_data;
 	WSAStartup(0x0201, &wsa_data);
 #endif
 	base = event_base_new();
+	printf("caijitest");
 	if (!base) {
 		fprintf(stderr, "Could not initialize libevent!\n");
+		return 1;
+	}
+	// 设置监听的地址端口
+	sockaddr_in.sin_family = AF_INET;
+	sockaddr_in.sin_port = htons(port);
+	listener = evconnlistener_new_bind(base, listener_cb, (void*)base,
+		LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_FREE, -1,
+		(struct sockaddr*)&sockaddr_in,
+		sizeof(sockaddr_in));
+	if (!listener)
+	{
+		std::cout << "could not create a listener\n";
 		return 1;
 	}
 	return 1;
---
+++ low_level_ROT13_with_libevent/low_level_ROT13_with_libevent.cpp
@@ -0,0 +1,58 @@
+﻿// low_level_ROT13_with_libevent.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <event2/event.h>
+const int MAX_LINE = 16384;
+
+void do_read(evutil_socket_t fd, short events, void* args);
+void do_write(evutil_socket_t fd, short events, void* args);
+
+char rot13_char(char c)
+{
+    /* We don't want to use isalpha here; setting the locale would change
+     * which characters are considered alphabetical. */
+    if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
+        return c + 13;
+    else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
+        return c - 13;
+    else
+        return c;
+}
+struct fd_state {
+    char buffer[MAX_LINE];
+    size_t buffer_used;
+    size_t n_written;
+    size_t write_upto;
+    struct event* read_event;
+    struct event* write_event;
+};
+
+void do_read(evutil_socket_t fd, short events, void* args)
+{
+
+}
+void do_write(evutil_socket_t fd, short events, void* args)
+{
+
+}
+void do_accept(evutil_socket_t, short events, void* arg)
+{
+
+}
+
+void run()
+{
+    evutil_socket_t listener;
+    struct sockaddr_in sin;
+    struct event_base* base;
+    struct event* listener_event;
+    base = event_base_new();
+}
+
+int main()
+{
+    run();
+    return 0;
+}
+
